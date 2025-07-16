0180edcc110ecd208be390b2921c70196ed31a21
abdallateef_shohdy <86281216+abdallateef-sa@users.noreply.github.com>
2025-07-16 21:12:42 +0300

    feat: remove uploads directory from .gitignore and update server to serve static files

--- src/server.js
+++ src/server.js
@@ -6,21 +6,19 @@ import ApiError from "./utils/ApiError.js";
 import globalError from "./middlewares/errorMiddleware.js";
 import dbConnection from "./config/database.js";
 import indexRoute from "./routes/indexRoute.js";
-import fs from 'fs';
-import path from 'path';
-
+import fs from "fs";
+import path from "path";
 
 // Create folders if not exist
-const imagePath = path.join('src', 'uploads', 'images');
-const pdfPath = path.join('src', 'uploads', 'pdfs');
+const imagePath = path.join("src", "uploads", "images");
+const pdfPath = path.join("src", "uploads", "pdfs");
 
-[imagePath, pdfPath].forEach(folder => {
+[imagePath, pdfPath].forEach((folder) => {
   if (!fs.existsSync(folder)) {
     fs.mkdirSync(folder, { recursive: true });
     console.log(`Created folder: ${folder}`);
   }
 });
-
 
 dotenv.config();
 
@@ -28,9 +26,12 @@ dotenv.config();
 dbConnection();
 
 const app = express();
-app.use(express.json({limit : '20kb'}));
+app.use(express.json({ limit: "20kb" }));
 app.use(cors());
 
+// Serve images and pdfs statically
+app.use('/api/v1/uploads/images', express.static(path.join(process.cwd(), 'src/uploads/images')));
+app.use('/api/v1/uploads/pdfs', express.static(path.join(process.cwd(), 'src/uploads/pdfs')));
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -38,7 +39,7 @@ if (process.env.NODE_ENV === "development") {
 }
 
 //Mount Routes
-app.use("/api/v1",indexRoute);
+app.use("/api/v1", indexRoute);
 
 app.use((req, res, next) => {
   next(new ApiError(`Can't find this route: ${req.originalUrl}`, 404));
